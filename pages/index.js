562726453ee2a242dae87c78bf1cb0b2081c818e
DanProença@DeviLab <devillabdeveloper@gmail.com>
2023-06-20 01:17:41 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   
   const [loading, setLoading]=useState(false)
   const [results, setResults] = useState([])
+  const [instances, setInstances] = useState([])
   const [instanceName, setInstaceName] = useState("")
   const [cpf, setCpf] = useState("")
   const [matrix, setMatrix] = useState("")
@@ -33,12 +34,28 @@ export default function Home() {
     setResults(json)
   }
 
+  async function getInstances(){
+    const res = await fetch('api/instances')
+    const json = await res.json()
+    setInstances(json)
+
+  }
+
+  async function deleteInstances(id){
+    const res = await fetch('api/instances', {method:"DELETE", body:JSON.stringify({id:id}),  headers: {
+      "Content-Type": "application/json",
+  }})
+    const json = await res.json()
+    setInstances(json)
+
+  }
+
   useEffect(()=>{
 
     setInterval(()=>{
-      getResults()
+      //getResults()
     },3000)
-
+    getInstances()
 
   },[])
 
@@ -66,15 +83,20 @@ return(
 <div>
   <label style={{display:"block"}} >Insira o CPF do Dono</label>
   <input style={{display:"block"}} onChange={(e)=>setCpf(e.target.value)} type="number"/>
-  <button onClick={()=>work()}>Teste</button>
+  <button onClick={()=>work()}>Nova Instancia</button>
 </div>
 
 
 
 
 </div>
-Resultados: {results.length}
-<textarea style={{width:"100%", height:"200px", overflow:"scroll", marginTop:"1em"}} value={results?.map(e=> e.number +"  "+ e.status).join('\n')}></textarea>
+<span style={{margin:"1em"}}>Resultados: <strong>{results.length}</strong></span>
+<textarea style={{width:"100%", height:"200px", overflow:"scroll", margin:"1em"}} value={results?.map(e=> [e.number.slice(0,8), e.number.slice(8,18), e.number.slice(18)].join(' ') +"  "+ e.status).join('\n')}></textarea>
+<br/>
+<div style={{display:"grid", gridTemplateColumns:"auto auto auto", columnGap:"1em" ,margin:"1em"}}>
+  {instances?.map(e=> <div style={{width:"100%", padding:"2em", textAlign:"center", border:"1px solid #000"}}><div><span><strong>{e.name}</strong></span></div><button style={{display:"block"}} onClick={()=>deleteInstances(e.id)}>Deletar</button></div>)}
+</div>
+
 
 </>
 )
